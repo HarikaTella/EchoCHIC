d24ce90108284433c03fd969258d549288e71b55
AL20123 <AL20123@ALIPL1916>
2023-11-20 12:00:23 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using EchoCHIC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EchoCHIC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult loginpage()
+        {
+            return View();
+        }
+        public IActionResult signuppage()
+        {
+            return View();
+        }
+        public IActionResult forgotpassword()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
